a72a7a92f73cd9becbe8ecfcc561e1caa25e3fc1
Digambar S <erdigambar@gmail.com>
2023-03-31 22:47:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const container=document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.Occupied');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect=document.getElementById('movie');
+populateUI();
+   let ticketPrice = +movieSelect.value;
+   function updateSelectedCount(){
+    const selectedSeats=document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(function(seat){
+        return[...seats].indexOf(seat);
+    });
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+    const selectedSeatsCount=selectedSeats.length;
+    count.innerText=selectedSeatsCount; 
+    total.innerText=selectedSeatsCount*ticketPrice;
+}
+function populateUI(){
+    const selectedSeats=JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats!==null && selectedSeats.length>0){
+        seats.forEach((seats,index)=>{
+            if(selectedSeats.indexOf(index)>-1){
+                seats.classList.add('selected'); 
+            }
+        });
+    }
+    const selectedMovieIndex=localStorage.getItem('selectedMovieIndex');
+    if (selectedMovieIndex !== null){
+        movieSelect.selectedIndex=selectedMovieIndex;
+    }
+}
+movieSelect.addEventListener('change',(e)=>{
+    ticketPrice=+e.target.value;
+    function setMovieData(movieIndex,moviePrice){
+        localStorage.setItem('selectedMovieIndex',movieIndex);
+        localStorage.setItem('selectedMoviePrice',moviePrice);
+    }
+    updateSelectedCount();
+});
+   
+   container.addEventListener('click',(e)=>{
+    if  (e.target.classList.contains('seat') && !e.target.classList.contains('Occupied')){
+        e.target.classList.toggle('selected');
+    }
+updateSelectedCount();
+   } );
+updateSelectedCount();
